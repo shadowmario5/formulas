0c7c0921ac00ddf46e7844ef4d62a031effe91ae
shadowmario5 <45501485+shadowmario5@users.noreply.github.com>
2018-11-30 18:09:25 -0600

    Add files via upload

---
+++ bloque 1 formula A/bloque 1 formula A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloque_1_formula_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, y;
+           
+            Console.Write("INGRESE EL VALOR DE A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("INGRESE EL VALOR DE B: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("INGRESE EL VALOR DE C: ");
+            c = int.Parse(Console.ReadLine());
+
+            y = (((a * c) - 5) / (7 * b)) - (9 * a);
+
+            Console.Write("EL VALOR DE Y ES " + y + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
